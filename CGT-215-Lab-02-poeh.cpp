4da6bd30e585357718cb3e7083a1c81ff844d794
Hannah Poe <poeh@purdue.edu>
2026-09-01 12:24:15 -0400

    Code added
    My modified code for finding x in the line equation plus the things to help it run

--- CGT-215-Lab-02-poeh.cpp
+++ CGT-215-Lab-02-poeh.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
-float A;
-float B;
-float X;
-cout << "Please enter a value for A: ";
-cin >> A;
-cout << "A=" << A << endl;
-B = (A * 4) + 1;
-cout << "And B=" << B << endl;
+    float A;
+    float B;
+    float X;
+    cout << "Please enter a value for A: ";
+    cin >> A;
+    cout << "Please enter a value for B: ";
+    cin >> B;
+    cout << "A=" << A << endl;
+    cout << "B=" << B << endl;
+    X = (-B/A);
+    cout << "x=" << X << endl;
 }
