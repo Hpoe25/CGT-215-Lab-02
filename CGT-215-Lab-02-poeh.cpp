f2ab76d03c2c0e15b58c2dc96a39de7a85e0b248
Hannah Poe <poeh@purdue.edu>
2026-09-01 12:12:36 -0400

    Update CGT-215-Lab-02-poeh.cpp

--- CGT-215-Lab-02-poeh.cpp
+++ CGT-215-Lab-02-poeh.cpp
@@ -1,1 +1,13 @@
-This is so the file can be submitted on brightspace
+#include <iostream>
+using namespace std;
+int main()
+{
+float A;
+float B;
+float X;
+cout << "Please enter a value for A: ";
+cin >> A;
+cout << "A=" << A << endl;
+B = (A * 4) + 1;
+cout << "And B=" << B << endl;
+}
